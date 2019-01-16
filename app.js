2e96a651570fee6c42a9437fa2e57ed30b07c904
john hsu <hsu@johns-Air.home>
2019-01-16 15:17:00 -0800

    finished react components

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    {props.groceryItem.map(Item =>
+      <GroceryListItem Item={Item}/>
+      )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    }
+    return(
+    <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.Item}</li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList groceryItem = {['Milk', 'Eggs']} />, document.getElementById("app"));
